0254249415658a41946e3b38a606d44b510b341c
julian nymark <julianymark@gmail.com>
2017-03-09 03:49:46 +0100

    STABLE VERSION!

--- js/magic.js
+++ js/magic.js
@@ -7,8 +7,6 @@ var fragmentShaderSource;
 var vertexShaderSource;
 
 function main() {
-  console.log('main');
-
   // canvas
   var canvas = document.createElement('canvas');
   canvas.id = 'thecanvas';
@@ -28,38 +26,23 @@ function main() {
 }
 
 function afterLoadingShaders() {
-  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fragmentShaderSource);
-  gl.compileShader(fragmentShader);
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.log('An error occurred compiling a shader: ' + gl.getShaderInfoLog(fragmentShader));
-  }
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 
-  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderSource);
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.log('An error occurred compiling a shader: ' + gl.getShaderInfoLog(vertexShader));
-  }
+  shaderProgram = createProgram(gl, vertexShader, fragmentShader);
 
-  var shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    console.log('Unable to link the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-  }
-
-  gl.useProgram(shaderProgram);
-
+  positionAttributeLocation = gl.getAttribLocation(shaderProgram, "a_position");
   squareVerticesBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
 
   var vertices = [
-    1.0, 1.0,  0.0,
-   -1.0, 1.0,  0.0,
-    1.0, -1.0, 0.0,
-   -1.0, -1.0, 0.0
+    1.0, 1.0,
+   -1.0, 1.0,
+    1.0, -1.0,
+
+   -1.0, 1.0,
+   -1.0, -1.0,
+    1.0, -1.0
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
@@ -68,50 +51,25 @@ function afterLoadingShaders() {
 }
 
 function drawScene() {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.useProgram(shaderProgram);
+  gl.enableVertexAttribArray(positionAttributeLocation);
 
-  perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
-
-  mvMatrix = Matrix.I(4);
-  mvTranslate([-0.0, 0.0, -6.0]);
-
+  // Bind the position buffer.
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-  setMatrixUniforms();
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-}
 
-function mvTranslate(v) {
-  multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
-}
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
-function setMatrixUniforms() {
-  var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-  gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
-
-  var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-  gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
-}
-
-function makePerspective(FOV, AspectRatio, Closest, Farest){
-  var YLimit = Closest * Math.tan(FOV * Math.PI / 360);
-  var A = -( Farest + Closest ) / ( Farest - Closest );
-  var B = -2 * Farest * Closest / ( Farest - Closest );
-  var C = (2 * Closest) / ( (YLimit * AspectRatio) * 2 );
-  var D = (2 * Closest) / ( YLimit * 2 );
-  return [
-    C, 0, 0, 0,
-    0, D, 0, 0,
-    0, 0, A, -1,
-    0, 0, B, 0
-  ];
-}
-
-function makeTransform(Object){
-  return [
-    1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    0, 0, -6, 1
-  ];
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 6;
+  gl.drawArrays(primitiveType, offset, count);
 }
--- shaders/shader.vert
+++ shaders/shader.vert
@@ -1,8 +1,5 @@
-attribute vec3 aVertexPosition;
-
-uniform mat4 uMVMatrix;
-uniform mat4 uPMatrix;
+attribute vec4 a_position;
 
 void main(void) {
-  gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+  gl_Position = a_position;
 }
